c208d5d3f91ebb154c81e9c0d853d4ce63949a49
Michael Sprague <mikesprague@gmail.com>
2021-09-14 00:33:41 +0000

    :construction: Add WIP

--- dist/index.js
+++ dist/index.js
@@ -507,23 +507,6 @@ exports.toCommandProperties = toCommandProperties;
 
 /***/ }),
 
-/***/ 312:
-/***/ ((module) => {
-
-let wait = function (milliseconds) {
-  return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
-      throw new Error('milliseconds not a number');
-    }
-    setTimeout(() => resolve("done!"), milliseconds)
-  });
-};
-
-module.exports = wait;
-
-
-/***/ }),
-
 /***/ 747:
 /***/ ((module) => {
 
@@ -590,20 +573,14 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(186);
-const wait = __nccwpck_require__(312);
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    // core.info(`Waiting ${ms} milliseconds ...`);
+    // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // core.debug((new Date()).toTimeString());
+    // core.info((new Date()).toTimeString());
+    // core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
