e2f62a1aa9133d424f87a7786d91dcd3b7605a8a
Michael Sprague <mikesprague@gmail.com>
2021-09-13 20:56:33 -0400

    :construction: More WIP

--- dist/index.js
+++ dist/index.js
@@ -576,6 +576,16 @@ const core = __nccwpck_require__(186);
 
 async function run() {
   try {
+    const title = core.getInput('title', {
+      trimWhitespace: true,
+      required: true,
+    });
+    const message = core.getInput('message', {
+      trimWhitespace: true,
+      required: false,
+    });
+    core.info(`Title: ${title}`);
+    core.info(`Message: ${message}`);
     // core.info(`Waiting ${ms} milliseconds ...`);
     // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // core.debug((new Date()).toTimeString());
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    const title = core.getInput('title', { required: true });
+    const title = core.getInput('title', {
+      trimWhitespace: true,
+      required: true,
+    });
+    const message = core.getInput('message', {
+      trimWhitespace: true,
+      required: false,
+    });
     core.info(`Title: ${title}`);
+    core.info(`Message: ${message}`);
     // core.info(`Waiting ${ms} milliseconds ...`);
     // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // core.debug((new Date()).toTimeString());
