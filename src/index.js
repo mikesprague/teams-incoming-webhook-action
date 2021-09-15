8aef7e056de41a3a66b748454cc6fb48a0c3697f
Michael Sprague <mikesprague@gmail.com>
2021-09-15 18:08:08 +0000

    :construction: Add WIP to to input collection

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ const core = require('@actions/core');
 
 async function run() {
   try {
+    const isDeployCard =
+      core.getBooleanInput('is-deploy-card', {
+        required: false,
+        trimWhitespace: true,
+      }) || false;
     const title = core.getInput('title', {
-      trimWhitespace: true,
       required: true,
+      trimWhitespace: true,
     });
     const message = core.getInput('message', {
-      trimWhitespace: true,
       required: false,
+      trimWhitespace: true,
     });
+    const color =
+      core.getInput('color', {
+        required: false,
+        trimWhitespace: true,
+      }) || '17a2b8';
+    core.info(`isDeployCard: ${isDeployCard}`);
     core.info(`Title: ${title}`);
     core.info(`Message: ${message}`);
+    core.info(`Color: ${color}`);
     // core.info(`Waiting ${ms} milliseconds ...`);
     // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // core.debug((new Date()).toTimeString());
