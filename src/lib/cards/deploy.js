74a221e3edf81b5c78cbd34df59a2eb6d2fe82fa
Michael Sprague <mikesprague@gmail.com>
2021-09-21 22:56:47 +0000

    :recycle: Rename export and use modern js

--- src/lib/cards/deploy.js
+++ src/lib/cards/deploy.js
@@ -1,6 +1,6 @@
-exports.deployCard = function ({
+exports.populateCard = ({
   title,
-  color = '17a2b8',
+  color,
   commit,
   branch,
   author,
@@ -10,43 +10,40 @@ exports.deployCard = function ({
   sha,
   repoUrl,
   timestamp,
-}) {
-  const messageCard = {
-    '@type': 'MessageCard',
-    '@context': 'https://schema.org/extensions',
-    summary: title,
-    themeColor: color,
-    title: title,
-    sections: [
-      {
-        activityTitle: `**Workflow Run [#${runNum}](${repoUrl}/actions/runs/${runId})** on [${repoName}](${repoUrl})`,
-        facts: [
-          {
-            name: 'Branch:',
-            value: `${branch}`,
-          },
-          {
-            name: 'Commit',
-            value: `${sha.substr(0, 7)}`,
-          },
-        ],
-        activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`,
-      },
-    ],
-    potentialAction: [
-      {
-        '@context': 'http://schema.org',
-        target: [`${repoUrl}/actions/runs/${runId}`],
-        '@type': 'ViewAction',
-        name: 'View Workflow Run',
-      },
-      {
-        '@context': 'http://schema.org',
-        target: [commit.data.html_url],
-        '@type': 'ViewAction',
-        name: 'View Commit Changes',
-      },
-    ],
-  };
-  return messageCard;
-};
+}) => ({
+  '@type': 'MessageCard',
+  '@context': 'https://schema.org/extensions',
+  summary: title,
+  themeColor: color,
+  title: title,
+  sections: [
+    {
+      activityTitle: `**Workflow Run [#${runNum}](${repoUrl}/actions/runs/${runId})** on [${repoName}](${repoUrl})`,
+      facts: [
+        {
+          name: 'Branch:',
+          value: `${branch}`,
+        },
+        {
+          name: 'Commit',
+          value: `${sha.substr(0, 7)}`,
+        },
+      ],
+      activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`,
+    },
+  ],
+  potentialAction: [
+    {
+      '@context': 'http://schema.org',
+      target: [`${repoUrl}/actions/runs/${runId}`],
+      '@type': 'ViewAction',
+      name: 'View Workflow Run',
+    },
+    {
+      '@context': 'http://schema.org',
+      target: [commit.data.html_url],
+      '@type': 'ViewAction',
+      name: 'View Commit Changes',
+    },
+  ],
+});
