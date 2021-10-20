7c578ffa0e6b6d4d7291a04db46024113f322787
Michael Sprague <ms388@cornell.edu>
2021-10-20 13:03:03 -0400

    :recycle: MessageCard to AdaptiveCard

--- src/lib/cards/deploy.js
+++ src/lib/cards/deploy.js
@@ -11,39 +11,74 @@ exports.populateCard = ({
   repoUrl,
   timestamp,
 }) => ({
-  '@type': 'MessageCard',
-  '@context': 'https://schema.org/extensions',
-  summary: title,
-  themeColor: color,
-  title: title,
-  sections: [
+  type: 'message',
+  attachments: [
     {
-      activityTitle: `**Workflow Run [#${runNum}](${repoUrl}/actions/runs/${runId})** on [${repoName}](${repoUrl})`,
-      facts: [
-        {
-          name: 'Branch:',
-          value: `${branch}`,
+      contentType: 'application/vnd.microsoft.card.adaptive',
+      content: {
+        type: 'AdaptiveCard',
+        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+        version: '1.3',
+        msteams: {
+          width: 'Full',
         },
-        {
-          name: 'Commit',
-          value: `${sha.substr(0, 7)}`,
-        },
-      ],
-      activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`,
-    },
-  ],
-  potentialAction: [
-    {
-      '@context': 'http://schema.org',
-      target: [`${repoUrl}/actions/runs/${runId}`],
-      '@type': 'ViewAction',
-      name: 'View Workflow Run',
-    },
-    {
-      '@context': 'http://schema.org',
-      target: [commit.data.html_url],
-      '@type': 'ViewAction',
-      name: 'View Commit Changes',
+        body: [
+          {
+            type: 'Container',
+            items: [
+              {
+                type: 'TextBlock',
+                text: title,
+                wrap: true,
+                size: 'Large',
+                weight: 'Bolder',
+              },
+            ],
+            style: color,
+            bleed: true,
+          },
+          {
+            type: 'TextBlock',
+            text: `**Workflow Run [#${runNum}](${repoUrl}/actions/runs/${runId})** on [${repoName}](${repoUrl})`,
+            wrap: true,
+          },
+          {
+            type: 'FactSet',
+            facts: [
+              {
+                title: 'Branch',
+                value: branch,
+              },
+              {
+                title: 'Commit',
+                value: `${sha.substr(0, 7)}`,
+              },
+            ],
+          },
+          {
+            type: 'TextBlock',
+            text: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`,
+            wrap: true,
+          },
+          {
+            type: 'ActionSet',
+            actions: [
+              {
+                type: 'Action.OpenUrl',
+                title: 'View Workflow Run',
+                url: `${repoUrl}/actions/runs/${runId}`,
+                style: 'default',
+              },
+              {
+                type: 'Action.OpenUrl',
+                title: 'View Commit Changes',
+                url: commit.data.html_url,
+                style: 'default',
+              },
+            ],
+          },
+        ],
+      },
     },
   ],
 });
