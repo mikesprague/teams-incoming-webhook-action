4b5e51582620c1d371343c858056d5ca40f64dc9
Michael Sprague <ms388@cornell.edu>
2021-10-20 12:14:13 -0400

    :recycle: Refactor MessageCard to AdaptiveCard

--- src/lib/cards/simple.js
+++ src/lib/cards/simple.js
@@ -1,8 +1,38 @@
-exports.populateCard = ({ title, message, color }) => ({
-  '@type': 'MessageCard',
-  '@context': 'https://schema.org/extensions',
-  summary: title,
-  themeColor: color,
-  title: title,
-  text: message,
+exports.messageCard = ({ title, text, color = 'emphasis' }) => ({
+  type: 'message',
+  attachments: [
+    {
+      contentType: 'application/vnd.microsoft.card.adaptive',
+      content: {
+        type: 'AdaptiveCard',
+        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+        version: '1.3',
+        msteams: {
+          width: 'Full',
+        },
+        body: [
+          {
+            type: 'Container',
+            items: [
+              {
+                type: 'TextBlock',
+                text: title,
+                wrap: true,
+                size: 'Large',
+                weight: 'Bolder',
+              },
+            ],
+            style: color,
+            bleed: true,
+          },
+          {
+            type: 'TextBlock',
+            text,
+            wrap: true,
+            height: 'stretch',
+          },
+        ],
+      },
+    },
+  ],
 });
