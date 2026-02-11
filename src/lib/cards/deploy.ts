a6e4f968cb5dfe05945e476dfbf0bb476ffcd615
Michael Sprague <mikesprague@gmail.com>
2026-02-11 14:24:54 -0500

    feat(lib/cards/deploy): add support for titleSize

--- src/lib/cards/deploy.ts
+++ src/lib/cards/deploy.ts
@@ -6,104 +6,110 @@ type GetCommitResponse =
   Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response'];
 
 export interface DeployCardParams {
-  title: string;
+  author: GetCommitResponse['data']['author'];
+  branch: string | undefined;
   color: string;
   commit: GetCommitResponse;
-  branch: string | undefined;
-  author: GetCommitResponse['data']['author'];
-  runNum: string;
-  runId: string;
   repoName: string;
-  sha: string;
   repoUrl: string;
+  runId: string;
+  runNum: string;
+  sha: string;
   timestamp: string;
+  title: string;
+  titleSize?: 'Default' | 'Large';
 }
 
 export const populateCard = ({
-  title,
+  author,
+  branch,
   color,
   commit,
-  branch,
-  author,
-  runNum,
-  runId,
   repoName,
-  sha,
   repoUrl,
+  runId,
+  runNum,
+  sha,
   timestamp,
-}: DeployCardParams) => ({
-  type: 'message',
-  attachments: [
-    {
-      contentType: 'application/vnd.microsoft.card.adaptive',
-      content: {
-        type: 'AdaptiveCard',
-        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
-        version: '1.3',
-        msteams: {
-          width: 'Full',
+  title,
+  titleSize = 'Large',
+}: DeployCardParams) => {
+  const workflowStatusCard = {
+    type: 'message',
+    attachments: [
+      {
+        contentType: 'application/vnd.microsoft.card.adaptive',
+        content: {
+          type: 'AdaptiveCard',
+          $schema: 'https://adaptivecards.io/schemas/adaptive-card.json',
+          version: '1.5',
+          msteams: {
+            width: 'Full',
+          },
+          body: [
+            {
+              type: 'Container',
+              items: [
+                {
+                  type: 'TextBlock',
+                  text: `${getEmoji(color)}${title}`,
+                  wrap: true,
+                  size: titleSize,
+                  weight: 'Bolder',
+                },
+              ],
+              style: color,
+              bleed: true,
+            },
+            {
+              type: 'TextBlock',
+              text: `**Workflow Run #${runNum}** on [${repoName}](${repoUrl})`,
+              wrap: true,
+            },
+            {
+              type: 'TextBlock',
+              text: `by ${commit.data.commit.author?.name ?? 'Unknown'}${
+                author?.login ? ` (@${author.login})` : ''
+              } on ${timestamp}`,
+              wrap: true,
+              size: 'Small',
+              spacing: 'None',
+            },
+            {
+              type: 'FactSet',
+              facts: [
+                {
+                  title: 'Branch',
+                  value: branch,
+                },
+                {
+                  title: 'Commit',
+                  value: `${sha.slice(0, 7)}`,
+                },
+              ],
+            },
+            {
+              type: 'ActionSet',
+              actions: [
+                {
+                  type: 'Action.OpenUrl',
+                  title: 'View Workflow Run',
+                  url: `${repoUrl}/actions/runs/${runId}`,
+                  style: 'default',
+                },
+                {
+                  type: 'Action.OpenUrl',
+                  title: 'View Commit Changes',
+                  url: commit.data.html_url,
+                  style: 'default',
+                },
+              ],
+            },
+          ],
         },
-        body: [
-          {
-            type: 'Container',
-            items: [
-              {
-                type: 'TextBlock',
-                text: `${getEmoji(color)}${title}`,
-                wrap: true,
-                size: 'Large',
-                weight: 'Bolder',
-              },
-            ],
-            style: color,
-            bleed: true,
-          },
-          {
-            type: 'TextBlock',
-            text: `**Workflow Run #${runNum}** on [${repoName}](${repoUrl})`,
-            wrap: true,
-          },
-          {
-            type: 'TextBlock',
-            text: `by ${commit.data.commit.author?.name ?? 'Unknown'}${
-              author?.login ? ` (@${author.login})` : ''
-            } on ${timestamp}`,
-            wrap: true,
-            size: 'Small',
-            spacing: 'None',
-          },
-          {
-            type: 'FactSet',
-            facts: [
-              {
-                title: 'Branch',
-                value: branch,
-              },
-              {
-                title: 'Commit',
-                value: `${sha.slice(0, 7)}`,
-              },
-            ],
-          },
-          {
-            type: 'ActionSet',
-            actions: [
-              {
-                type: 'Action.OpenUrl',
-                title: 'View Workflow Run',
-                url: `${repoUrl}/actions/runs/${runId}`,
-                style: 'default',
-              },
-              {
-                type: 'Action.OpenUrl',
-                title: 'View Commit Changes',
-                url: commit.data.html_url,
-                style: 'default',
-              },
-            ],
-          },
-        ],
       },
-    },
-  ],
-});
+    ],
+  };
+
+  return workflowStatusCard;
+};
